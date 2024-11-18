3c464c4390f4e78a8769b37229a75e0a8d3ef6b6
OlekZakhar <olekzakhar@gmail.com>
2024-11-18 12:28:50 +0200

    [+] Return link to main page zapys

--- app/page.js
+++ app/page.js
@@ -6,21 +6,25 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-start">
         <h1 className="text-3xl">aurorarent.net</h1>
         <ol className="list-inside list-decimal text-sm text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-8 py-5 px-4 relative border border-white/30">            
+          <li className="mb-8 py-5 px-4 relative border border-white/30">
             <h2 className="inline-block underline underline-offset-8">jatke.co - WhatsApp Catalog Builder</h2>
             <p className="mt-4 ml-6 max-w-xl">
               Create WhatsApp catalog with Google Sheets Data. Make Ordering on WhatsApp super easy. Your customers will love this.
             </p>
             <Link href="https://jatke.co" className="absolute top-0 left-0 w-full h-full"></Link>
           </li>
-          <li className="mb-8 py-5 px-4 relative border border-white/30">            
+          <li className="mt-16 mb-8 py-5 px-4 relative border border-white/30">
+            <Link href="https://zapys.co" className="absolute -top-[42px] left-3">
+              <h2 className="inline-block text-xl">MicroTools</h2>
+            </Link>
+
             <h2 className="inline-block underline underline-offset-8">AVIF to JPG converter</h2>
             <p className="mt-4 ml-6 max-w-xl">
               Quickly and easily convert AVIF images to JPG with our reliable AVIF to JPG converter. Maintain image quality while ensuring compatibility with all devices and platforms. Perfect for seamless image optimization—try it today!
             </p>
             <Link href="https://zapys.co/avif-to-jpg" className="absolute top-0 left-0 w-full h-full"></Link>
           </li>
-          <li className="mb-8 py-5 px-4 relative border border-white/30">            
+          <li className="mb-8 py-5 px-4 relative border border-white/30">
             <h2 className="inline-block underline underline-offset-8">AVIF to PNG converter</h2>
             <p className="mt-4 ml-6 max-w-xl">
               Easily convert AVIF files to PNG format with our fast and reliable AVIF to PNG converter. Preserve quality and transparency while ensuring compatibility with all your projects. Try it now!
